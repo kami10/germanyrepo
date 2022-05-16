1bba1e92dfec68444d096d86b716383f4608a650
kamran Barakpour <kamran_barakpour@yahoo.com>
2022-05-16 16:05:40 +0430

    2 new files-htaccess and src folder

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 echo 'hello kamran';
+
+$address = $_GET['path'];
+
+echo $address;
+
+switch ($address){
+    case 'contact':
+        include 'src/Controllers/contact.php';
+        break;
+    case 'about':
+        include 'about.php';
+        break;
+    default:
+        echo 'home page';
+}
+?>
+
+<ul>
+    <a href="src/Controllers/contact.php"><li>contact</li></a>
+    <li>about</li>
+    <li>home</li>
+</ul>
